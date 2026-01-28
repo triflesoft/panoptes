01e0e35ac0de9fe87959d9a478507f3e995583a5
Roman <roman@adontz.com>
2026-01-28 20:37:51 +0400

    Initial commit.

---
+++ provision/provision.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S uv run --script --refresh --python "3.15"
+# /// script
+# dependencies = [
+#   "Jinja2",
+# ]
+# ///
+
+print("----------------")
---
+++ provision/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#/usr/local/bin/uv run --script --refresh --python "3.15" /opt/panoptes/provision/provision.py
+
+ls -laZ /opt/
+ls -laZ /etc/
